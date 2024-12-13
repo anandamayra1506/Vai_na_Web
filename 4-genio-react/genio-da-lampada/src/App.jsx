0e8dfed5f5c0f9a5c79bdd603ad82a6d4dbbea55
Ananda Máyra <anandacursosi@gmail.com>
2024-12-12 23:35:57 -0300

    Atualizando o projeto Gênio da Lâmpada

--- 4-genio-react/genio-da-lampada/src/App.jsx
+++ 4-genio-react/genio-da-lampada/src/App.jsx
@@ -6,52 +6,51 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Sevillana", cursive;
+  color: white;
 }
+
 body{
-    background-image: url("https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/Imagem%20palacio.png?raw=true");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100vw 100vh;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    font-family: "Sevillana", cursive;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/Imagem%20palacio.png?raw=true");
 }
-
 `;
-
+const Main = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: center;
+  align-items: center;
+`;
 const Titulo = styled.h1`
-  color:#FFFFFF;
-  font-family: Sevillana;
-  font-size: 62px;
+  font-size: 64px;
   font-weight: 400;
-  line-height: 84.13px;
-  text-align: left;
-  text-underline-position: from-font;
-  text-decoration-skip-ink: none;
-
+  line-height: 5.258rem;
 `;
 
 const Imagem = styled.img`
-  width: 741px;
-  height: 691px;
-  top: 157px;
-  left: 351px;
-  gap: 0px;
-  opacity: 0px;
-  display: block;
-  background-color: #007BFF;
-  border: none;
-  color: white;
-  padding: 10px 20px;
-  font-size: 24px;
-  border-radius: 30px;
-  cursor: pointer;
+  width: 60vw;
+  height: 60vh;
 `;
 
+const Button = styled.button`
+  background-color: #007BFF;
+  border: none;
+  font-size: 20px;
+  border-radius: 30px;
+  width: 250px;
+  height: 50px;
+  padding: 9px 50px;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #B74794;
+  }
+`
 
 export default function App(){
   const [lampada, setLampada] = useState(false);
@@ -60,14 +59,15 @@ export default function App(){
   };
 
   return (
-    <main>
+    <Main>
       <GlobalStyle/>
         <Titulo>Liberte a magia da programação com a lâmpada de Aladim.</Titulo>
         <Imagem 
           src={
             lampada ? "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada-com-mago.png?raw=true" : "https://github.com/mariaccarolina/TurmaFDV/blob/main/imagens/lampada.png?raw=true"} alt="Lâmpada"
             onClick={trocarLampada}
-        />
-    </main>
+        /> 
+        <Button onClick={trocarLampada} >Clique Aqui</Button> 
+    </Main>
   );
 }
